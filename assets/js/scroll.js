a932b3d316bd853d51c0c54e90b01ae706401dd7
JamesSwatton <JamesSwatton@users.noreply.github.com>
2022-03-31 20:00:20 +0000

    deploy: 52c79e63b55b02fd8fc7cc0a450202a069c37036

---
+++ assets/js/scroll.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+	console.log('DOM fully loaded and parsed');
+
+	window.onscroll = function() {scrollFunction()};
+
+	var logoLg = document.getElementById("logo--lg")
+	var logoMd = document.getElementById("logo--md")
+	var miniBio = document.getElementById("mini-bio")
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 10 ||
+			document.documentElement.scrollTop > 10) {
+			logoLg.style.fontSize = "2em"
+			logoMd.style.fontSize = "2em"
+			miniBio.style.opacity = "0%"
+			miniBio.style.transform = "translate(0, -1em)"
+			miniBio.style.height = "0"
+		} else {
+			logoLg.style.fontSize = "4em"
+			logoMd.style.fontSize = "3em"
+			miniBio.style.opacity = "100%"
+			miniBio.style.transform = "translate(0, 0)"
+			miniBio.style.height = "auto"
+		}
+	}
+});
