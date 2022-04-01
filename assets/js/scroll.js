91373dad1d3ce930cc1ff463f681b8496ea1619f
JamesSwatton <jamessswatton@gmail.com>
2022-04-01 17:48:52 +0100

    fix site name transition

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 	var logoLg = document.getElementById("logo--lg")
 	var logoMd = document.getElementById("logo--md")
+	var logoSm = document.getElementById("logo--sm")
 	var miniBio = document.getElementById("mini-bio")
 
 	function scrollFunction() {
@@ -12,12 +13,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
 			document.documentElement.scrollTop > 10) {
 			logoLg.style.fontSize = "2em"
 			logoMd.style.fontSize = "2em"
+			logoSm.style.fontSize = "1.5em"
 			miniBio.style.opacity = "0%"
 			miniBio.style.transform = "translate(0, -1em)"
 			miniBio.style.height = "0"
 		} else {
 			logoLg.style.fontSize = "4em"
 			logoMd.style.fontSize = "3em"
+			logoSm.style.fontSize = "2em"
 			miniBio.style.opacity = "100%"
 			miniBio.style.transform = "translate(0, 0)"
 			miniBio.style.height = "auto"
--- css/config.js
+++ css/config.js
@@ -7,7 +7,7 @@ module.exports = {
 		shsm: "0px 10px 20px"
 	},
 	breakPoints: {
-    'sm': '@media(min-width:500px)', // breakpoint 1
+    'sm': '@media(min-width:600px)', // breakpoint 1
     'md': '@media(min-width:900px)', // breakpoint 2
     'lg': '@media(min-width:1200px)' // breakpoint 3
 }
